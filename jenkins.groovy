37dee913bff7df41fe8310cbb8c579b9ae2e8ea2
Katelyn Gigante <clockwork.singularity@gmail.com>
2017-06-02 21:25:51 +1000

    Update stages

--- jenkins.groovy
+++ jenkins.groovy
@@ -3,7 +3,7 @@ node {
     stage('checkout'){
         checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '61235359-4c9e-4d64-b63a-7717e51f3069', url: 'https://github.com/PennyDreadfulMTG/modo-bugs.git']]])
     }
-    stage('build') {
+    stage('Scrape') {
         sh 'python3 -m pip install --user -r requirements.txt'
         withCredentials([usernamePassword(credentialsId: 'd61f34a1-4929-406d-b4c5-ec380d823780', passwordVariable: 'github_password', usernameVariable: 'github_user')]) {
             sh 'git config user.email "jenkins@katelyngigante.com"'
@@ -11,12 +11,16 @@ node {
             sh 'git checkout master'
             sh 'git pull'
             sh 'python3 scrape_bugblog.py'
-            sh 'python3 update.py'
-            def updated = sh returnStatus: true, script: 'git diff --exit-code'
-            if (updated){
-                sh 'git commit -a -m "Updated Bug List from Issues"'
-            }
-            sh 'git push https://$github_user:$github_password@github.com/PennyDreadfulMTG/modo-bugs.git'
         }
+    }
+    stage('Update'){
+        sh 'python3 update.py'
+        def updated = sh returnStatus: true, script: 'git diff --exit-code'
+    }
+    stage('Push changes'){
+        if (updated){
+            sh 'git commit -a -m "Updated Bug List from Issues"'
+        }
+        sh 'git push https://$github_user:$github_password@github.com/PennyDreadfulMTG/modo-bugs.git'
     }
 }
