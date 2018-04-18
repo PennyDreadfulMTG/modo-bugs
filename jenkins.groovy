76780744923acbad514ac2e3560d19077b7586c1
Katelyn Gigante <clockwork.singularity@gmail.com>
2018-04-18 13:57:03 +1000

    Turns out we hit a really weird race condition.

--- jenkins.groovy
+++ jenkins.groovy
@@ -31,6 +31,7 @@ node('linux') {
         def updated = sh returnStatus: true, script: 'git diff --exit-code'
         if (updated){
             sh 'git commit -a -m "Updated Bug List from Issues"'
+            sh 'git pull'
         }
         withCredentials([usernamePassword(credentialsId: 'd61f34a1-4929-406d-b4c5-ec380d823780', passwordVariable: 'github_password', usernameVariable: 'github_user')]) {
             sh 'git push https://$github_user:$github_password@github.com/PennyDreadfulMTG/modo-bugs.git'
